176166830639f563ab1775d07fce9529345f1d05
KristePe <biskyte@hotmail.com>
2023-08-03 21:40:21 +0300

    DoughnutChart added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Inputs from './components/inputs/Inputs';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { library } from '@fortawesome/fontawesome-svg-core'
+ 
+
+
 
 library.add();
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <ChartWrapper/> */}
       <Inputs />
     </div>
   );
